8c7d493ead1b32b7926cf736ae5c380fd8d4fe6e
Suhan Nath <snath6@uic.edu>
2020-04-16 23:02:22 -0500

    :sparkles: Async changes

--- src/App.js
+++ src/App.js
@@ -5,43 +5,118 @@ const prompt = require("prompt-sync")({ sigint: true });
 const copydir = require("copy-dir");
 const mv = require("mv");
 
-const copyEarmoFolder = new Promise((resolve, reject) => {
-  copydir(
-    "./earmo_files/earmo_executable/",
-    "./Output/earmo_executable/",
-    {
-      utimes: true, // keep add time and modify time
-      mode: true, // keep file mode
-      cover: true, // cover file when exists, default is true
-    },
-    function (err) {
-      if (err) return reject(err);
-      return resolve("Copying Done");
-    }
-  );
-});
-
-const runBatchFiles = new Promise((resolve, reject) => {
-  var bat = require.resolve("../earmo_files/dex2jar-2.0/d2j-dex2jar.bat");
-  var apk = require.resolve("../earmo_files/dex2jar-2.0/ensichat_17.apk");
-  var ls = spawn(bat, [apk]);
-
-  ls.stdout.on("data", (data) => {
-    console.log(data.toString());
+const copyEarmoFolderFunc = () => {
+  const copyEarmoFolder = new Promise((resolve, reject) => {
+    copydir(
+      "./earmo_files/earmo_executable/",
+      "./Output/earmo_executable/",
+      {
+        utimes: true, // keep add time and modify time
+        mode: true, // keep file mode
+        cover: true, // cover file when exists, default is true
+      },
+      function (err) {
+        if (err) return reject(err);
+        return resolve("Copying Done");
+      }
+    );
   });
 
-  ls.stderr.on("data", (data) => {
-    console.error(data.toString());
+  return copyEarmoFolder;
+};
+
+const runBatchFilesFunc = () => {
+  const runBatchFiles = new Promise((resolve, reject) => {
+    var bat = require.resolve("../earmo_files/dex2jar-2.0/d2j-dex2jar.bat");
+    var apk = require.resolve("../earmo_files/dex2jar-2.0/ensichat_17.apk");
+    var ls = spawn(bat, [apk]);
+
+    ls.stdout.on("data", (data) => {
+      console.log(data.toString());
+    });
+
+    ls.stderr.on("data", (data) => {
+      console.error(data.toString());
+    });
+
+    ls.on("exit", (code) => {
+      console.log(`Child exited with code ${code}`);
+    });
   });
 
-  ls.on("exit", (code) => {
-    return resolve(`Child exited with code ${code}`);
+  return runBatchFiles;
+};
+
+const getIndependentRunsCountFunc = () => {
+  const getIndependentRunsCount = new Promise((resolve, reject) => {
+    const independentRuns = prompt("Number of independent runs: ");
+
+    lineReplace({
+      file: "./RefactoringStandarStudyAndroid.prop",
+      line: 26,
+      text: `independentRuns=${independentRuns}`,
+      addNewLine: true,
+      callback: ({ file, line, text, replacedText }) => {
+        return resolve("Entered" + independentRuns);
+      },
+    });
   });
-});
+
+  return getIndependentRunsCount;
+};
+
+const getEOValueFunc = () => {
+  const getEOValue = new Promise((resolve, reject) => {
+    const e0 = prompt("Enter E0 value: ");
+
+    lineReplace({
+      file: "./RefactoringStandarStudyAndroid.prop",
+      line: 30,
+      text: `originalAppEnergyUsage=${e0}`,
+      addNewLine: true,
+      callback: ({ file, line, text, replacedText }) => {
+        return resolve("Entered" + e0);
+      },
+    });
+  });
+
+  return getEOValue;
+};
+
+const moveJarFileFunc = () => {
+  const moveJarFile = new Promise((resolve, reject) => {
+    mv(
+      "./ensichat_17-dex2jar.jar",
+      "./Output/earmo_executable/ensichat_17-dex2jar.jar",
+      function (err) {
+        return reject("Moving jar file" + err);
+      }
+    );
+  });
+  return moveJarFile;
+};
+
+const movePropFileFunc = () => {
+  const movePropFile = new Promise((resolve, reject) => {
+    fs.copyFile(
+      "./RefactoringStandarStudyAndroid.prop",
+      "./Output/earmo_executable/RefactoringStandarStudyAndroid.prop",
+      function (error) {
+        return reject(console.log(error));
+      }
+    );
+  });
+
+  return movePropFile;
+};
 
 const callAutomator = async () => {
-  const earmoMvresp = await copyEarmoFolder;
-  const runBtcFiles = await runBatchFiles;
+  const getIdptRunsCount = await getIndependentRunsCountFunc();
+  const getEOVal = await getEOValueFunc();
+  const earmoMvresp = await copyEarmoFolderFunc();
+  const mvJarFile = await moveJarFileFunc();
+  const mvPropFile = await movePropFileFunc();
+  const runBtcFiles = await runBatchFilesFunc();
 };
 
 callAutomator()
@@ -51,45 +126,3 @@ callAutomator()
   .catch((error) => {
     console.log(error);
   });
-
-// const independentRuns = prompt("Number of independent runs: ");
-
-// lineReplace({
-//   file: "./RefactoringStandarStudyAndroid.prop",
-//   line: 26,
-//   text: `independentRuns=${independentRuns}`,
-//   addNewLine: true,
-//   callback: ({ file, line, text, replacedText }) => {
-//     const e0 = prompt("E0 value: ");
-//     replaceE0(e0);
-//   },
-// });
-
-// const replaceE0 = (e0) => {
-//   lineReplace({
-//     file: "./RefactoringStandarStudyAndroid.prop",
-//     line: 30,
-//     text: `originalAppEnergyUsage=${e0}`,
-//     addNewLine: true,
-//     callback: ({ file, line, text, replacedText }) => {
-//       moveFilesForEarmo();
-//     },
-//   });
-// };
-
-// const moveFilesForEarmo = () => {
-//   mv(
-//     "./ensichat_17-dex2jar.jar",
-//     "./Output/earmo_executable/ensichat_17-dex2jar.jar",
-//     function (err) {
-//       console.log(err);
-//     }
-//   );
-//   fs.copyFile(
-//     "./RefactoringStandarStudyAndroid.prop",
-//     "./Output/earmo_executable/RefactoringStandarStudyAndroid.prop",
-//     function (error) {
-//       console.log(error);
-//     }
-//   );
-// };
